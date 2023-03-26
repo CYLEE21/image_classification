7c67316cccc0c4d92e29303b94202bcf15f95c8f
CYLEE21 <Brianlee881@gmail.com>
2023-03-26 17:13:30 +0200

    modify readme and add images

---
+++ README.md
@@ -0,0 +1,33 @@
+# image_classification
+
+This the repo for some classification codes.
+
+## Project highlight
+
+Duration: 1 weeks
+
+Languages: Python
+
+Top used libraries: Keras, openCV, pandas, numpy.
+
+Models: (1) Stacked CNN, (2) resNet50
+
+Dataset: (1) online sources, (2) mnist dataset.
+
+IDE: VScode, Windows 10, Mac OS, git
+
+## Project objective and result
+
+Objective: classify the given pictures.
+
+Result: The model can classify the given pictures with simple CNN models.
+
+## Visualization
+As we can see the machine can classify the images between wolf and wolfdog.
+
+![result_1](https://github.com/CYLEE21/image_classification/blob/master/binary_classification/result_1.jpg?raw=true)
+
+But there are something still can be improved, if the image is from some other types of wolf, the machine will be confused. This might be improved if we use more various types of wolf and wolfdog images to train the model. 
+
+![result_2](https://github.com/CYLEE21/image_classification/blob/master/binary_classification/result_2.jpg?raw=true)
+
--- binary_classification/main.py
+++ binary_classification/main.py
@@ -7,31 +7,33 @@ import imghdr
 import matplotlib.pyplot as plt
 import cv2
 import tensorflow as tf
-data_dir = 'E:\img_data'
 
-image_exts = ['jpeg', 'jpg', 'bmp', 'png']
+# load the data
+data_dir = 'E:\img_data'
 dog_img = os.listdir(os.path.join(data_dir, 'dog'))
 cat_img = os.listdir(os.path.join(data_dir, 'cat'))
 
 #organize the image_dataset into classes
 #dataset folder -> classes folders
-data = tf.keras.utils.image_dataset_from_directory('E:\img_data')
-
+# therefore, we use tf dataset to auto loading images.
+data = tf.keras.utils.image_dataset_from_directory(data_dir)
+# put into the numpy iterator
 data_iterator = data.as_numpy_iterator()
+# it will iterate the batch every execution
 batch = data_iterator.next()
-#should be 2
+#should be 2 batch[0] is images batch[1] is labels
 len(batch)
 #(32, 256, 256, 3) as defult, image
 batch[0].shape
 #(32,) as defult, labels
 batch[1].shape
-
+#plot the image
 fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
 for idx, img in enumerate(batch[0][:4]):
     ax[idx].imshow(img.astype(int))
     ax[idx].title.set_text(batch[1][idx])
 plt.show()
-#1 is wolfdog, 0 is wolf
+#0 is wolf, 1 is wolfdog
 
 #scaled all the data from [0,1]
 data = data.map(lambda x,y: (x/255, y))
@@ -43,22 +45,26 @@ data.as_numpy_iterator().next()[0].min()
 len(data)
 
 #split the data
-train_size = int(len(data)*.7)
-val_size = int(len(data)*.2)+1
-test_size = int(len(data)*.1)+1
+
+train_size = int(round(len(data)*.7))
+val_size = int(round(len(data)*.2))+1
+test_size = int(round(len(data)*.1))
 train_size+val_size+test_size
 
 train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
 test = data.skip(train_size+val_size).take(test_size)
 
-
+#build the model
 model = Sequential()
 model.add(Conv2D(16, (3,3), 1, input_shape=(256, 256, 3)))
+model.add(BatchNormalization())
 model.add(MaxPool2D())
 model.add(Conv2D(16, (3,3), 1))
+model.add(BatchNormalization())
 model.add(MaxPool2D())
 model.add(Conv2D(16, (3,3), 1))
+model.add(BatchNormalization())
 model.add(MaxPool2D())
 model.add(Flatten())
 model.add(Dense(1, activation='sigmoid'))
@@ -67,14 +73,8 @@ model.summary()
 
 hist = model.fit(train, epochs=50, validation_data=val)
 
-test_model = tf.keras.applications.resnet50.ResNet50()
-
-
-img = cv2.imread('labura.jpg')
-plt.imshow(img)
-plt.show()
-
-resize = tf.image.resize(img, (224, 224))
+img = cv2.imread('C:/Users/Brian/image_classification/binary_classification/wolfdog_3.jpg')
+resize = tf.image.resize(img, (256, 256))
 plt.imshow(resize.numpy().astype(int))
 plt.show()
 #[256, 256, 3]
@@ -87,9 +87,13 @@ inputs = np.expand_dims(resize/255, 0)
 pred = model.predict(inputs)
 pred
 
+#using resNet50 for the saliency map
+test_model = tf.keras.applications.resnet50.ResNet50()
+resize = tf.image.resize(img, (224, 224))
+inputs = np.expand_dims(resize/255, 0)
 pred_resnet = test_model.predict(inputs)
 pred_resnet
-
+#plot saliency map
 max_idx = tf.argmax(pred_resnet)
 with tf.GradientTape() as tape:
     images = tf.Variable(inputs, dtype=float)
@@ -103,53 +107,8 @@ def normalize_image(img):
     grads_norm = img[:,:,0]+ img[:,:,1]+ img[:,:,2]
     grads_norm = (grads_norm - tf.reduce_min(grads_norm))/ (tf.reduce_max(grads_norm)- tf.reduce_min(grads_norm))
     return grads_norm
-plt.imshow(normalize_image(grads[0]), cmap='ocean')
+plt.imshow(normalize_image(grads[0]), cmap='coolwarm')
 plt.show()
 
 plt.imshow(normalize_image(images[0]))
 plt.show()
-
-
-
-
-
-
-images = tf.Variable(inputs, dtype=float)
-
-with tf.GradientTape() as tape:
-    images = tape.watch(images)
-    pred = model(images, training=False)
-    class_idxs_sorted = np.argsort(pred.numpy().flatten())[::-1]
-    loss = pred[0][class_idxs_sorted[0]]
-    
-grads = tape.gradient(loss, images)
-dgrad_abs = tf.math.abs(grads)
-dgrad_max_ = np.max(dgrad_abs, axis=3)[0]
-arr_min, arr_max  = np.min(dgrad_max_), np.max(dgrad_max_)
-grad_eval = (dgrad_max_ - arr_min) / (arr_max - arr_min + 1e-18)
-
-fig, axes = plt.subplots(1,2,figsize=(14,5))
-axes[0].imshow(resize.numpy().astype(int))
-i = axes[1].imshow(grad_eval,cmap="jet",alpha=0.8)
-fig.colorbar(i)
-plt.show()
-
-
-
-
-pred = model.predict(test)
-
-y_pred_list = []
-y_true_list = []
-
-for images, labels in test.take(1):
-    numpy_images = images.numpy()
-    numpy_labels = labels.numpy()
-len(numpy_labels)
-len(pred)
-for pred, true in zip(pred, numpy_labels):
-  y_pred_list.append(np.argmax(pred))
-  y_true_list.append(np.argmax(true))
-
-from sklearn.metrics import confusion_matrix as cm
-print(cm(y_true_list, y_pred_list))
